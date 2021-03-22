85df6793ef877c42d74f5693b5e72061a9d1f349
Tanmay Patil <tanmaypatil@L-C02DL366P3YV.local>
2021-03-22 21:18:00 +0530

    sonar scanner setup

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ export default function App() {
 			{/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
 			<Switch>
-				<Route path="/about">
-					<About />
-				</Route>
-				<Route path="/users">
-					<Users />
-				</Route>
 				<Route path="/">
 					<Home />
 				</Route>
@@ -23,12 +17,4 @@ export default function App() {
 
 function Home() {
 	return <h2>Home</h2>;
-}
-
-function About() {
-	return <h2>About</h2>;
-}
-
-function Users() {
-	return <h2>Users</h2>;
 }
