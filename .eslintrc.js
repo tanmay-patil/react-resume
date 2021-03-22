ad70608151bc01ea36a56c7ee42940abe422085f
Tanmay Patil <tanmaypatil@L-C02DL366P3YV.local>
2021-03-22 21:10:28 +0530

    setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+
+/** Follow this article for eslint-prettier setup : https://morioh.com/p/5fd9a1794ed3 */
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		// suppress errors for missing 'import React' in files
+		'react/react-in-jsx-scope': 'off',
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
+};
