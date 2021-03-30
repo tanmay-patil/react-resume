c1637f0cad51cefc758f39a803581f9f5ec0af28
Tanmay Patil <tanmaypatil@L-C02DL366P3YV.local>
2021-03-30 23:58:48 +0530

    add spring animation simple opacity

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,32 @@
+/* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types';
 import './index.scss';
 import { emptyArrowFunction } from '../../helpers/utils';
 import TopBar from '../../containers/top-bar';
 import ThemeWrapper from '../../containers/theme-toggle/ThemeWrapper';
+import { useSpring, animated } from 'react-spring';
+import { useEffect, useState } from 'react';
 export default function Home() {
+	const [showName, setShowName] = useState(false);
+	const props = useSpring({ opacity: showName ? 1 : 0 });
+
+	useEffect(() => {
+		setTimeout(() => {
+			setShowName((val) => {
+				return !val;
+			});
+		}, 1000);
+	}, []);
+
 	return (
 		<>
 			<ThemeWrapper>
 				<TopBar />
 			</ThemeWrapper>
 
-			<div className="container">TANMAY PATIL</div>
+			<animated.div style={props}>
+				<div className="container">TANMAY PATIL</div>
+			</animated.div>
 		</>
 	);
 }
